3e28145cb4c4ce6c7c3cc3a3c6301e07a59bdd32
geekidentity <geekidentity@163.com>
2023-05-12 10:50:41 +0800

    node-exporter

---
+++ node-exporter/run.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+function usage() {
+    echo "usage: $0 start|stop|down|restart|status|front"
+}
+
+function network_config() {
+    case $1 in
+    init)
+        ;;
+    status)
+        docker network ls
+        ;;
+    remove)
+        ;;
+    *)
+        ;;
+    esac
+}
+
+SERVICE=$2
+
+case $1 in
+start)
+    docker-compose up -d $SERVICE
+    ;;
+stop)
+    docker-compose stop $SERVICE
+    ;;
+down)
+    docker-compose down
+    ;;
+restart)
+    docker-compose restart $SERVICE
+    ;;
+front)
+    docker-compose up $SERVICE
+    ;;
+status)
+    docker-compose ps $SERVICE
+    ;;
+*)
+    usage
+    exit 3
+esac
